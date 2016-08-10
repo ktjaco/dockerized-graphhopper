e7d77d866248ddc63d0b84627de6d74125739866
ktjaco <ktjaco@users.noreply.github.com>
2016-08-10 00:59:52 -0400

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+GRAPHHOPPER_DIR=/graphhopper
+DATA_DIR=/data
+TEMP_DIR=/tmp/
+TEMP_GRAPH_DIR=~/tmp/graphhopper/
+
+mkdir $GRAPHHOPPER_DIR
+cd $GRAPHHOPPER_DIR
+
+#wget https://oss.sonatype.org/content/groups/public/com/graphhopper/graphhopper-web/0.3/graphhopper-web-0.3-bin.zip
+wget https://oss.sonatype.org/content/groups/public/com/graphhopper/graphhopper-web/0.7.0/graphhopper-web-0.7.0-bin.zip
+unzip *.zip
+rm *.zip
+
+mv /tmp/config.properties $GRAPHHOPPER_DIR
+mv /tmp/start.sh $GRAPHHOPPER_DIR
+
+echo "Showing contents of ${GRAPHHOPPER_DIR}"
+ls $GRAPHHOPPER_DIR
+
+echo "Showing contents of ${DATA_DIR}"
+ls $DATA_DIR
+
+echo "Showing contents of ${TEMP_DIR}"
+ls $TEMP_DIR
+
---
+++ start.sh
@@ -0,0 +1,41 @@
+#!/bin/sh
+
+GRAPHHOPPER_DIR=/graphhopper
+DATA_DIR=/data
+TEMP_DIR=/tmp/
+TEMP_GRAPH_DIR=~/tmp/graphhopper/
+
+mkdir -p ~/tmp/graphhopper/
+cd ~/tmp/graphhopper/
+wget http://download.geofabrik.de/europe/monaco-latest.osm.pbf
+
+echo "Showing contents of ${GRAPHHOPPER_DIR}"
+ls $GRAPHHOPPER_DIR
+
+echo "Showing contents of ${DATA_DIR}"
+ls $DATA_DIR
+
+echo "Showing contents of ${TEMP_DIR}"
+ls $TEMP_DIR
+
+echo "Showing contents of ${TEMP_GRAPH_DIR}"
+ls $TEMP_GRAPH_DIR
+
+OSM_FILE=`ls /data/*.pbf`
+
+if [ -f /data/env.sh  ]; then
+    . /data/env.sh
+fi
+
+if [ -z "${JAVA_OPTS}" ]; then
+    JAVA_OPTS="$JAVA_OPTS -Xms64m -Xmx1024m -XX:MaxPermSize=256m -Djava.net.preferIPv4Stack=true"
+    JAVA_OPTS="$JAVA_OPTS -server -Djava.awt.headless=true -Xconcurrentio"
+    echo "Setting default JAVA_OPTS"
+fi
+
+RUN_ARGS=" -jar /graphhopper/*.jar jetty.resourcebase=/graphhopper/webapp config=/graphhopper/config.properties osmreader.osm=$OSM_FILE"
+
+echo "JAVA_OPTS= ${JAVA_OPTS}"
+echo "RUN_ARGS= ${RUN_ARGS}"
+
+java $JAVA_OPTS $RUN_ARGS
